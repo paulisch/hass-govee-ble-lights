ad3d082ff30a3f2d2f699e8ad930cdbec4a7b41c
Paul Schmutz <paul@powerus.de>
2024-10-23 23:50:02 +0200

    Try to fix color command

--- custom_components/hass-govee-ble-lights/light.py
+++ custom_components/hass-govee-ble-lights/light.py
@@ -79,7 +79,7 @@ class GoveeBluetoothLight(LightEntity):
 
         if ATTR_RGB_COLOR in kwargs:
             red, green, blue = kwargs.get(ATTR_RGB_COLOR)
-            await self._sendBluetoothData(LedCommand.COLOR, [LedMode.MANUAL, 0x01, red, green, blue, 0x00,  0x00, 0x00, 0x00, 0x00, 0xFF, 0x7F])
+            await self._sendBluetoothData(LedCommand.COLOR, [LedMode.MANUAL, red, green, blue, 0x00, 0xFF, 0xAE, 0x54])
 
     async def async_turn_off(self, **kwargs) -> None:
         await self._sendBluetoothData(LedCommand.POWER, [0x0])
